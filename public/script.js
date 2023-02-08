5c127655dc236edaaa42c9f2e9307b1b36b88f13
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-02-08 11:28:04 +0100

    V.2

--- public/script.js
+++ public/script.js
@@ -1,8 +1,23 @@
-function myFunction() {
-  var x = document.getElementById("card2");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+let card = document.querySelector(".card");
+let svg = document.querySelector("svg");
+
+svg.addEventListener("click", toggle);
+
+function toggle() {
+  card.classList.toggle("back");
+}
+
+const cardflip = document.querySelector(".front");
+
+if (cardflip) {
+  window.addEventListener("mousemove", (event) => {
+    const xPercent = event.clientX / window.innerWidth - 0.5;
+    const yPercent = event.clientY / window.innerHeight - 0.5;
+    cardflip.style.transform = `rotateX(${yPercent * 40}deg) rotateY(${
+      xPercent * 40
+    }deg)`;
+  });
+  window.addEventListener("mouseout", () => {
+    cardflip.style.transform = `rotateX(0) rotateY(0)`;
+  });
 }
