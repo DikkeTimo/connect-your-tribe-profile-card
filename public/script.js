7de1ff2fa9216aa58a946d76075275b8048d0fb7
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-02-10 12:59:26 +0100

    V.5

--- public/script.js
+++ public/script.js
@@ -13,8 +13,8 @@ if (cardflip) {
   window.addEventListener("mousemove", (event) => {
     const xPercent = event.clientX / window.innerWidth - 0.5;
     const yPercent = event.clientY / window.innerHeight - 0.5;
-    cardflip.style.transform = `rotateX(${yPercent * 40}deg) rotateY(${
-      xPercent * 40
+    cardflip.style.transform = `rotateX(${yPercent * 60}deg) rotateY(${
+      xPercent * 60
     }deg)`;
   });
   window.addEventListener("mouseout", () => {
